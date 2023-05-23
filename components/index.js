1c98af1284ccadb1e896ba66ddac44f15a24f92a
Soulpurr <92094765+Soulpurr@users.noreply.github.com>
2023-05-23 22:34:21 +0530

    done

---
+++ components/index.js
@@ -0,0 +1,31 @@
+import About from "./About";
+import Experience from "./Experience";
+import Contact from "./Contact";
+import Feedback from "./Feedback";
+import Hero from "./Hero";
+import Loader from "./Loader";
+import Navbar from "./Navbar";
+import Tech from "./Tech";
+import Work from "./Work";
+import CanvasLoader from "./Canvas/CanvasLoader";
+import EarthCanvas from "./Canvas/EarthCanvas";
+import BallCanvas from "./Canvas/BallCanvas";
+import ComputerCanvas from "./Canvas/ComputerCanvas";
+import StarsCanvas from "./Canvas/StarsCanvas";
+
+export {
+  Loader,
+  Hero,
+  Navbar,
+  About,
+  Tech,
+  Experience,
+  Work,
+  Feedback,
+  Contact,
+  CanvasLoader,
+  EarthCanvas,
+  BallCanvas,
+  ComputerCanvas,
+  StarsCanvas,
+};
